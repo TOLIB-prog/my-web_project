5b354fdc008f084a1f06ee769235276cd783e0f4
Tolib Ibadullaev <abutolibibodulloev@gmail.com>
2025-06-08 02:00:08 +0300

    Добавил несколько строк кода

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-console.log('Hello my friend!');
+// Дополнительный эффект: изменение скорости при наведении
+const heading = document.querySelector('.logo')
+
+heading.addEventListener('mouseenter', () => {
+  heading.computedStyleMap.animationDuration = '3.5s'
+})
+
+heading.addEventListener('mouseleave', () => {
+  heading.computedStyleMap.animationDuration = '2.5s'
+})
