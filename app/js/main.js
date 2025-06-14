ddcefb5fe57c6bfdedd6361a29be448c1ee6b6fb
Tolib Ibadullaev <abutolibibodulloev@gmail.com>
2025-06-14 23:34:32 +0300

    Settings

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,25 @@
 // Дополнительный эффект: изменение скорости при наведении
-const heading = document.querySelector('.logo')
+// const heading = document.querySelector('.logo')
 
-heading.addEventListener('mouseenter', () => {
-  heading.computedStyleMap.animationDuration = '3.5s'
-})
+// heading.addEventListener('mouseenter', () => {
+//   heading.computedStyleMap.animationDuration = '3.5s'
+// })
 
-heading.addEventListener('mouseleave', () => {
-  heading.computedStyleMap.animationDuration = '2.5s'
-})
+// heading.addEventListener('mouseleave', () => {
+//   heading.computedStyleMap.animationDuration = '2.5s'
+// })
+
+// document.querySelectorAll('.menu__link').forEach((menu__link) => {
+//   // Для постоянной анимации уже добавлен класс constant-animation
+// })
+var price = 23.99;
+var discount = 10;
+var total = price * (discount / 100);
+
+console.log("The total price of the one check: ", total);
+
+var temp = 10;
+console.log((9 / 5) * temp + 32);
+
+var user_name = "Martha";
+console.log(user_name + ", " + "you've won!");
